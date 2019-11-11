7f266c95c3f4a65b1e5731734c42626bc07d4a1a
Aleksalencar <47511195+Aleksalencar@users.noreply.github.com>
2019-11-11 00:14:31 -0300

    Reformulçao de Objetos para classe Main
    Button -> String

--- src/hotel/boundary/Main.java
+++ src/hotel/boundary/Main.java
@@ -16,94 +16,88 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 
+public class Main extends Application implements EventHandler<Event>, Executor {
 
-public class Main extends Application implements EventHandler<Event>, Executor{
-
+	private TilePane pane = new TilePane();
 	private VBox box = new VBox();
 	private Label titulo = new Label("Menu principal");
-	private Button btnHosp = new Button("Hospedagens");
-	private Button btnQuarto = new Button("Quartos");
-	private Button btnCliente = new Button("Clientes");
-	private Button btnFunc = new Button("Funcionarios");
-	private Button btnPromo = new Button("Promoções");
-	private Button btnEstoque = new Button("Estoque");
-	//private Button btnMenu = new Button("Menu principal");
+	private String btnHosp = "Hospedagens";
+	private String btnQuarto = "Quartos";
+	private String btnCliente = "Clientes";
+	private String btnFunc = "Funcionarios";
+	private String btnPromo = "Promoções";
+	private String btnEstoque = "Estoque";
 	private Map<String, BoundaryContent> telas = new HashMap<>();
 
 	public static void main(String[] args) {
 		launch(args);
 	}
+
 	@Override
 	public void start(Stage stage) throws FileNotFoundException {
-		box.setPrefSize(700.0, 650.0);
-		box.setAlignment(Pos.CENTER);
-		box.setSpacing(20); // 2
-		box.addEventHandler(ActionEvent.ANY, this);
-		titulo.setTextAlignment(TextAlignment.CENTER);
-		titulo.setFont(new Font(27));
-		box.getChildren().add(titulo);
-	
-		//box.setStyle("-fx-background-color: linear-gradient( from 0% 0% to 100% 100%, white 0%, silver 100%);");
-		gerartelas();
-		Scene scn = new Scene(box);
+		pane.setPrefSize(700.0, 650.0);
+		pane.setAlignment(Pos.CENTER);
+		pane.addEventHandler(ActionEvent.ANY, this);
+
+		comandoTelas();
+		pane.getChildren().add(box);
+		Scene scn = new Scene(pane);
 		stage.setScene(scn);
 		stage.show();
 		executar("Menu principal");
 
 	}
-	public void gerartelas() throws FileNotFoundException {
 
-		// gerar as telas 
-	    telas.put("Menu principal", new MenuPrincipalBoundary(this));
-		telas.put(btnFunc.getText(), new FuncionarioBoundary(this));
-		telas.put(btnCliente.getText(), new ClienteBoundary(this));
-		telas.put(btnPromo.getText(), new PromocoesBoundary(this));
-		telas.put(btnEstoque.getText(), new EstoqueBoundary(this));
+	public void comandoTelas() throws FileNotFoundException {
+
+		// gerar as telas
+		telas.put("Menu principal", new MenuPrincipalBoundary(this));
+		telas.put(btnFunc, new FuncionarioBoundary(this));
+		telas.put(btnCliente, new ClienteBoundary(this));
+		telas.put(btnPromo, new PromocoesBoundary(this));
+		telas.put(btnEstoque, new EstoqueBoundary(this));
 	}
-
 
 	@Override
 	public void handle(Event event) {
 		if (event.getTarget() instanceof Button) {
-			Button n = (Button)event.getTarget();
+			Button n = (Button) event.getTarget();
 			String comando = n.getText();
 			executar(comando);
 		}
 
 	}
-	public void definirBackground(String imageName) throws FileNotFoundException{
-		String path="src/hotel/images/"+imageName+".jpg";
+
+	public void definirBackground(String imageName) throws FileNotFoundException {
+		String path = "src/hotel/images/" + imageName + ".jpg";
 		System.out.println(path);
 		FileInputStream imagem = new FileInputStream(path);
-		System.out.println("path:"+imagem);
-		Image image = new Image(imagem); 
-		BackgroundSize size = new BackgroundSize(box.getWidth(),box.getHeight(),true,true,true,true);
-	    BackgroundImage backgroundimage = new BackgroundImage(
-	    		image,  
-	            BackgroundRepeat.NO_REPEAT,  
-	            BackgroundRepeat.NO_REPEAT,  
-	            BackgroundPosition.DEFAULT,  
-	            size)
-	    		;
-        Background background = new Background(backgroundimage); 
-        box.setBackground(background);
+		System.out.println("path:" + imagem);
+		Image image = new Image(imagem);
+		BackgroundSize size = new BackgroundSize(box.getWidth(), box.getHeight(), true, true, true, true);
+		BackgroundImage backgroundimage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT,
+				BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, size);
+		Background background = new Background(backgroundimage);
+		pane.setBackground(background);
 	}
 
 	@Override
 	public void executar(String cmd) {
 		System.out.println("Executando comando " + cmd);
 		BoundaryContent tela = telas.get(cmd);
-		if (tela != null) { 
+		if (tela != null) {
 			box.getChildren().clear();
 			try {
 				definirBackground(cmd);
@@ -113,7 +107,5 @@ public class Main extends Application implements EventHandler<Event>, Executor{
 			box.getChildren().add(tela.gerarTela());
 		}
 	}
-
-
 
 }
